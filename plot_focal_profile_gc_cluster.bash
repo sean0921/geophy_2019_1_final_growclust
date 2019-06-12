d2f6fa81aef5d5767eb3d16f192dd395618a6072
holishing <holishingtest@gmail.com>
2019-06-12 23:12:59 +0800

    set up edge width of circle and star

--- plot_focal_profile_gc_cluster.bash
+++ plot_focal_profile_gc_cluster.bash
@@ -22,16 +22,16 @@ gmt psscale -C"$cpt" -Dx14/0+w9/.5+e -Ba500+l"Elevation (m)" -O -K >> "$ps"
 
 # plot Earthquakes (cluster)
 #awk '{print $9,$8,$11/12}' "$catalog" | gmt psxy -R -JM -Sc -K -O >> "$ps"
-awk '{if (NF==22 && $1==1) print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -Wred -K -O >> "$ps"
-awk '{if (NF==22 && $1==2) print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -Wblue -K -O >> "$ps"
-awk '{if (NF==22 && $1==3) print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -Wgreen -K -O >> "$ps"
-awk '{if (NF==22 && $1>3)  print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -W195/214/25 -K -O >> "$ps"
+awk '{if (NF==22 && $1==1)   print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -W1,red -K -O >> "$ps"
+awk '{if (NF==22 && $1==2)   print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -W1,blue -K -O >> "$ps"
+awk '{if (NF==22 && $1==1.5) print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -W1,green -K -O >> "$ps"
+awk '{if (NF==22 && $1>1.5)  print $12,$11,$4/12}' "$clust" | gmt psxy -R -JM -Sc -W1,195/214/25 -K -O >> "$ps"
 
 # plot focal mechanism
 gmt psmeca 201807030920.foc -R -JM -Sa1.0/14p/6 -Gred -O -K >> "$ps"
 
 # plot centroid of earthquakes cluster
-awk '{if (NF==6 && $1<4) print $4,$3,0.3}' "$clust" | gmt psxy -R -JM -Sa -Wpurple -Gyellow -K -O >> "$ps"
+awk '{if (NF==6 && $1<4) print $4,$3,0.3}' "$clust" | gmt psxy -R -JM -Sa -W1,purple -Gyellow -K -O >> "$ps"
 
 # plot AA' line
 # coord. of A1 & A2
@@ -86,30 +86,30 @@ gmt psbasemap -R0/"$mdis"/0/"$depth" -JX15/-7.5 -BwESn -Bxa+l"Distance (km)" -By
 #awk '{printf("%f %f %f %f\n",$9,$8,$10,$11)}' "$clust" | \
 awk '{if(NF==22 && $1==1) printf("%f %f %f %f\n",$12,$11,$13,$4)}' "$clust" | \
 gmt project -C"$A1_lon"/"$A1_lat" -E"$A2_lon"/"$A2_lat" -W-"$width"/"$width" -Q > catalog_profile.gmt
-awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -Wred -K -O >> "$ps"
+awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -W1,red -K -O >> "$ps"
 
 # cluster No.2
 #awk '{printf("%f %f %f %f\n",$9,$8,$10,$11)}' "$clust" | \
 awk '{if(NF==22 && $1==2) printf("%f %f %f %f\n",$12,$11,$13,$4)}' "$clust" | \
 gmt project -C"$A1_lon"/"$A1_lat" -E"$A2_lon"/"$A2_lat" -W-"$width"/"$width" -Q > catalog_profile.gmt
-awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -Wblue -K -O >> "$ps"
+awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -W1,blue -K -O >> "$ps"
 
 # cluster No.3
 #awk '{printf("%f %f %f %f\n",$9,$8,$10,$11)}' "$clust" | \
 awk '{if(NF==22 && $1==3) printf("%f %f %f %f\n",$12,$11,$13,$4)}' "$clust" | \
 gmt project -C"$A1_lon"/"$A1_lat" -E"$A2_lon"/"$A2_lat" -W-"$width"/"$width" -Q > catalog_profile.gmt
-awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -Wgreen -K -O >> "$ps"
+awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -W1,green -K -O >> "$ps"
 
 # other clusters
 #awk '{printf("%f %f %f %f\n",$9,$8,$10,$11)}' "$clust" | \
 awk '{if(NF==22 && $1>3) printf("%f %f %f %f\n",$12,$11,$13,$4)}' "$clust" | \
 gmt project -C"$A1_lon"/"$A1_lat" -E"$A2_lon"/"$A2_lat" -W-"$width"/"$width" -Q > catalog_profile.gmt
-awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -W195/214/25 -K -O >> "$ps"
+awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sc -W1,195/214/25 -K -O >> "$ps"
 
 # plot centroid of earthquakes cluster
 awk '{if (NF==6 && $1<4) printf("%f %f %f %f\n",$4,$3,$5,6)}' "$clust" | \
 gmt project -C"$A1_lon"/"$A1_lat" -E"$A2_lon"/"$A2_lat" -W-"$width"/"$width" -Q > catalog_profile.gmt
-awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sa -Wpurple -Gyellow -K -O >> "$ps"
+awk '{print $5,$3,$4/12}' catalog_profile.gmt | gmt psxy -R -JX -Sa -W1,purple -Gyellow -K -O >> "$ps"
 
 #==== plot the focal mechanism
 gmt project 201807030920.foc -C"$A1_lon"/"$A1_lat" -E"$A2_lon"/"$A2_lat" -W-"$width"/"$width" -Q > focal_profile.gmt
@@ -127,11 +127,11 @@ gmt psxy -R -J -O -K -W0.05c,black >> "$ps" << BOXEND
 1 1
 BOXEND
 
-echo "2 2 0.5" | gmt psxy -R -J -Sc -Wred -K -O >> "$ps"
-echo "2 3 0.5" | gmt psxy -R -J -Sc -Wblue -K -O >> "$ps"
-echo "2 4 0.5" | gmt psxy -R -J -Sc -Wgreen -K -O >> "$ps"
-echo "2 5 0.5" | gmt psxy -R -J -Sc -W195/214/25 -K -O >> "$ps"
-echo "2 6 0.5" | gmt psxy -R -J -Sa -Wpurple -Gyellow -K -O >> "$ps"
+echo "2 2 0.5" | gmt psxy -R -J -Sc -W1,red -K -O >> "$ps"
+echo "2 3 0.5" | gmt psxy -R -J -Sc -W1,blue -K -O >> "$ps"
+echo "2 4 0.5" | gmt psxy -R -J -Sc -W1,green -K -O >> "$ps"
+echo "2 5 0.5" | gmt psxy -R -J -Sc -W1,195/214/25 -K -O >> "$ps"
+echo "2 6 0.5" | gmt psxy -R -J -Sa -W1,purple -Gyellow -K -O >> "$ps"
 echo "3 2 Cluster 1" | gmt pstext -R -J -O -K -F+f14p+jML >> "$ps"
 echo "3 3 Cluster 2" | gmt pstext -R -J -O -K -F+f14p+jML >> "$ps"
 echo "3 4 Cluster 3" | gmt pstext -R -J -O -K -F+f14p+jML >> "$ps"
